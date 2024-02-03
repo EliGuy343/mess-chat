db98ee79ac9ab15f906e89d6a15aa7645e24d8eb
Eli Guy <eliguy343@gmail.com>
2024-02-03 20:03:53 +0200

    layout bullshit

---
+++ app/(auth)/register/page.jsx
@@ -0,0 +1,10 @@
+import Form from "@components/Form"
+
+
+const Register = () => {
+  return (
+    <Form/>
+  )
+}
+
+export default Register
---
+++ components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='auth'>
+      <div className='content'>
+        <img src='/assets/logo.png' alt='logo' className='logo'/>
+      </div>
+    </div>
+  )
+}
+
+export default Form
