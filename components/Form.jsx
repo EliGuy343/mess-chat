40400f1df0b3962a7a88d51a51f52254a1843da3
Eli Guy <eliguy343@gmail.com>
2024-02-06 21:44:22 +0200

    useform

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,25 +1,66 @@
+"use client"  
 import { EmailOutlined, LockOutlined, Person, PersonOutline } from '@mui/icons-material'
 import Link from 'next/link'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const Form = ({type}) => {
+  const {
+    register, 
+    handleSumbit,
+    watch,
+    formState: {errors},
+  } = useForm();
+
   return (
     <div className='auth'>
       <div className='content'>
         <img src='/assets/logo.png' alt='logo' className='logo'/>
         <form className='form'>
           {type == "register" && (
-            <div className='input'>
-              <input  type='text' placeholder='Username'  className='input-field'/>
-              <PersonOutline sx={{color: "#737373"}}/>
-            </div>
+            <>
+              <div className='input'>
+                <input
+                  type='text'
+                  placeholder='Username'
+                  className='input-field'
+                  {...register("username", {required:"username is required"})}
+                />
+                <PersonOutline sx={{color: "#737373"}}/>
+              </div>
+              {errors.email && (
+                <p className='text-red-500'>{errors.email.message}</p>
+              )}
+            </>
           )}
-          <div className='input'>
-            <input type='text' placeholder='Username' className='input-field'/>
-            <EmailOutlined sx={{color: "#737373"}}/>
-          </div>
+          <>
+            <div className='input'>
+              <input
+                type='text'
+                placeholder='Email'
+                className='input-field'
+                {...register("email",{required:"Email is required"})}
+              />
+              <EmailOutlined sx={{color: "#737373"}}/>
+            </div>
+            {errors.username && (
+                <p className='text-red-500'>{errors.username.message}</p>
+            )}
+          </>
           <div  className='input'>
-            <input  type='password' placeholder='Password'  className='input-field'/>
+            <input
+              type='password'
+              placeholder='Password' 
+              className='input-field'
+              {...register("password", {
+                  required:"Password is required",
+                  validate:(value) => {
+                    if(value.length < 6) {
+                      return "Password needs to be at least 6 characters long"
+                    }
+                  }
+              })}
+            />
             <LockOutlined sx={{color: "#737373"}}/>
           </div>
           <button className='button'  type='sumbit'>
