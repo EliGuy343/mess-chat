19f77155569a027f279a01894d6cbde6831a2265
Eli Guy <eliguy343@gmail.com>
2024-02-14 23:07:08 +0200

    user update

--- app/(root)/profile/page.jsx
+++ app/(root)/profile/page.jsx
@@ -1,7 +1,9 @@
 "use client"
 
+import Loader from '@components/Loader'
 import { Person2Outlined } from '@mui/icons-material'
 import { useSession } from 'next-auth/react'
+import { CldUploadButton } from 'next-cloudinary'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -11,11 +13,30 @@ const Profile = () => {
   const {
     register,
     watch,
+    setValue,
+    reset,
     handleSubmit,
     formState: {error}
   } = useForm();
+  const [loading, setLoading] = useState(true);
   
-  return (
+  useEffect(() => {
+    if(user) {
+      reset({
+        username: user?.username,
+        profileImage: user?.profileImage
+      })
+    }
+    setLoading(false);
+  }, []);
+
+
+
+  const uploadPhoto = (result) => {
+    setValue("profileImage", result?.info?.secure_url);
+  }
+
+  return loading ? <Loader/> : (
     <div className='profile-page'>
       <h1 className='text-heading3-bold'>Edit Your Profile</h1>
       <form className='edit-profile'>
@@ -37,15 +58,21 @@ const Profile = () => {
         </div>
         <div className='flex items-center justify-between'>
           <img
-            src={user?.profileImage || "/assets/person.jpg"}
+            src={watch("profileImage") || user?.profileImage || "/assets/person.jpg"}
             alt='profile picture'
             className='w-40 h-40 rounded-full'
-          />
-          <p
-            className='text-body-bold'
+            />
+          <CldUploadButton
+            options={{maxFiles: 1}}
+            onUpload={uploadPhoto}
+            uploadPreset='npczwv3c'
           >
-            Upload New Photo
-          </p>
+            <p
+              className='text-body-bold'
+            >
+              Upload New Photo
+            </p>
+          </CldUploadButton>
         </div>
         <button
           className='btn'
