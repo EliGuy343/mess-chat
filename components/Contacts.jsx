5d90c0a70317965a56e86dec34a6aa0670688f1a
Eli Guy <eliguy343@gmail.com>
2024-02-16 17:15:14 +0200

    get contacts api

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -1,4 +1,30 @@
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+
 const Contacts = () => {
+  const [loading, setLoading] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const {data: session} = useSession();
+  const currentUser = session?.user;
+
+  const getContacts = async () => {
+    try {
+      const res = await fetch('/api/users');
+      const data = await res.json();
+      setContacts(data.filter((contact) => contact._id !== currentUser._id));
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  useEffect(() => {
+    if(currentUser) {
+      getContacts();
+    }
+  }, [currentUser]);
+
+
   return (
     <div className="create-chat-container">
       <input placeholder="Search contact..." className="input-search"/>
