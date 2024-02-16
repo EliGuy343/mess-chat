cc67228f883be710ecbc4318e1ba126df1ec60bc
Eli Guy <eliguy343@gmail.com>
2024-02-16 19:09:23 +0200

    contacts

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -1,5 +1,9 @@
+"use client"
+
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Loader from "./Loader";
+import { RadioButtonUnchecked } from "@mui/icons-material";
 
 const Contacts = () => {
   const [loading, setLoading] = useState(false);
@@ -11,6 +15,7 @@ const Contacts = () => {
     try {
       const res = await fetch('/api/users');
       const data = await res.json();
+      console.log(data);
       setContacts(data.filter((contact) => contact._id !== currentUser._id));
       setLoading(false);
     } catch (err) {
@@ -25,9 +30,30 @@ const Contacts = () => {
   }, [currentUser]);
 
 
-  return (
+  return loading ? <Loader/> : (
     <div className="create-chat-container">
       <input placeholder="Search contact..." className="input-search"/>
+      <div className="contact-bar">
+        <div className="contact-list">
+          <p className="text-body-bold">
+            Select Or Deselect
+          </p>
+          {contacts.map((user, index) =>(
+            <div className="contact" key={index}>
+              <RadioButtonUnchecked/>
+              <img src={user.profileImage || "/assets/person.jpg"} className="profilePhoto"/>
+              <p className="text-bold-bold">
+                {user.username}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="create-chat">
+          <button className="btn">
+            Start A New Chat
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
