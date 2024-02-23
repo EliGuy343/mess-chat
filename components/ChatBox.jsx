ff770a0791a3be2f2eeacb49a15a18e9d8bca1f8
Eli Guy <eliguy343@gmail.com>
2024-02-23 15:04:03 +0200

    chats box

---
+++ components/ChatBox.jsx
@@ -0,0 +1,41 @@
+import {format} from 'date-fns';
+const ChatBox = ({chat, currentUser}) => {
+
+  const otherMembers = chat?.members?.filter(
+    (member) => member._id !== currentUser._id
+  );
+  const lastMessage = chat?.messages?.length > 0
+    && chat?.messages[chat?.messages.length - 1];
+
+  return (
+    <div className="chat-box">
+      <div className="chat-info">
+        {chat?.isGroup ? (
+          <img 
+            src={chat?.groupPhoto || "/assets/group.jpg"}
+            alt="group-pic"
+            className="profilePhoto"
+          />
+        ) : (
+          <img
+            src={otherMembers[0].profilePhoto||"/assets/person.jpg"}
+            alt=""
+            className="profilePhoto"
+          />
+        )}
+        <div className="flex flex-col gap-1">
+          {chat?.isGroup ? (
+            <p className="text-base-bold">{chat?.name}</p>
+          ) : (
+            <p className="text-base-bold">{otherMembers[0]?.username}</p>
+          )}
+        </div>
+        <p className="text-base-light text-gray-3">
+          {!lastMessage && format(new Date(chat?.createdAt), "p")}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default ChatBox
--- components/ChatList.jsx
+++ components/ChatList.jsx
@@ -1,7 +1,41 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react";
+import Loader from "./Loader";
+import ChatBox from "./ChatBox";
+
 const ChatList = () => {
-  return (
+  const {data: sessions} = useSession();
+  const currentUser = sessions?.user;
+
+  const [loading, setLoading] = useState(true);
+  const [chats, setChats] = useState([]);
+
+  const getChats = async () => {
+    try {
+      const res = await fetch(`/api/users/${currentUser._id}`);
+    const data = await res.json();
+    setChats(data);
+    setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    if(currentUser) {
+      getChats()
+    }
+
+  }, [currentUser]);
+
+  return loading ? <Loader/> : (
     <div className="chat-list">
       <input placeholder="search chat..." className="input-search" />
+      {chats.map((chat, index) => (
+        <ChatBox chat={chat} index={index} currentUser={currentUser}/>
+      ))}
     </div>
   )
 }
