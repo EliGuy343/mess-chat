0505539fef96232cdcd5d6627b82ed08e347e4bf
Eli Guy <eliguy343@gmail.com>
2024-02-27 13:32:55 +0200

    get chat details api

---
+++ app/api/chats/[chatId]/route.js
@@ -0,0 +1,19 @@
+import Chat from "@models/Chat";
+import User from "@models/User";
+import { connectToDB } from "@mongodb";
+
+export const GET = async (req, {params}) => {
+  const {chatId} = params;
+  try {
+    await connectToDB();
+    const chat = await Chat.findById(chatId).populate({
+      path:"members",
+      model: User,
+    }).exec();
+
+    return new Response(JSON.stringify(chat), {status: 200});
+  } catch (err) {
+    console.log(err);
+    return new Response("Failed to get chat details", {status: 500});
+  }
+}
--- components/ChatDetails.jsx
+++ components/ChatDetails.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
 
-const ChatDetails = () => {
+
+const ChatDetails = ({chatId}) => {
+  const [loading, setLoading] = useState(true);
+  const [chat, setChat] = useState({});
+  const [otherMembers, setOtherMembers] = useState([]);
+
+  const getChatDetails = async () => {
+    try {
+      const res = await fetch(
+        `/api/chats/${chatId}`,
+        {
+          method:"GET",
+          headers:{
+            "Content-Type":"application/json"
+          }
+      });
+      const data = await res.json();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
   return (
     <div>ChatDetails</div>
   )
