637ee4fe621cf14bd6ba72edfca6610c8b082dc9
Eli Guy <eliguy343@gmail.com>
2024-02-28 22:31:04 +0200

    i'm tired

--- components/ChatDetails.jsx
+++ components/ChatDetails.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Loader from "./Loader";
+import Link from "next/link";
 
 
 const ChatDetails = ({chatId}) => {
@@ -17,14 +19,50 @@ const ChatDetails = ({chatId}) => {
           }
       });
       const data = await res.json();
+      setChat(data);
+      setOtherMembers(data?.members?.filter(member => member._id !== currentUser._id));
+      setLoading(false);
     } catch (err) {
       console.log(err);
     }
   }
-  
-  return (
-    <div>ChatDetails</div>
-  )
-}
+
+  useEffect(() => {
+    if(currentUser && chatId) {
+      getChatDetails();
+    }
+  }, [currentUser, chatId])
+  return loading ? <Loader/> : (
+    <div className="chat-details">
+      <div className="chat-header">
+        {chat?.isGroup ? (
+          <Link>
+            <img
+              src={chat?.groupPhoto || "/assets/group.png"}
+              alt=""
+              className="profilePhoto"
+            />
+            <div className="text">
+              <p>
+                {chat?.name} &#160; &#183; &#160; {chat?.members?.length}
+              </p>
+            </div>
+          </Link>
+        ) : (
+          <>
+            <img
+              src={otherMembers[0].profileImage || "/assets/person.jpg"}
+              alt=""
+              className="profilePhoto"
+            />
+            <div className="text">
+              <p>{otherMembers[0].username}</p>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default ChatDetails
