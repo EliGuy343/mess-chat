9739ca3540b06178f765e90002e28103af169fd4
Eli Guy <eliguy343@gmail.com>
2024-03-01 11:59:47 +0200

    Message model

---
+++ models/Message.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  chat: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"chat",
+  },
+  sender :{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  },
+  text: {
+    type: String,
+    default: ""
+  },
+  photo: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  seenBy: {
+    type: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    default: []
+  }  
+});
+
+const message = mongoose.models.Message || mongoose.model("Message", messageSchema);
+
+export default message;
