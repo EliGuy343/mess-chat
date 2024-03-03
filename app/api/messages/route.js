39f039e66a37d64f344edb7176a933c20166d74d
Eli Guy <eliguy343@gmail.com>
2024-03-03 22:59:20 +0200

    populate

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -1,3 +1,5 @@
+import Chat from "@models/Chat";
+import Message from "@models/Message";
 import { connectToDB } from "@mongodb";
 
 export const POST = async () => {
@@ -5,8 +7,34 @@ export const POST = async () => {
     await connectToDB();
     const body = req.json()
     const {chatId, currentUserId, text, photo} = body;
-  
-  } catch (error) {
     
+    const newMessage = await Message.create({
+      chat:chatId,
+      sender: currentUserId,
+      text,
+      photo,
+      seenBy: currentUserId
+    });
+
+    const updatedChat = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        $push: {Message: newMessage._id},
+        $set: {lastMessageAt: newMessage.createdAt},
+      },
+      {new: true}
+    ).populate({
+      path: "messages",
+      model: Message,
+      populate: {path: "sender seenBy", model: "User"}
+    })
+    .populate({
+      path: "members",
+      model: "User"
+    })
+    .exec();
+    return new Response(JSON.stringify(newMessage), {status:200});
+  } catch (err) {
+    return new Response("Internal Server Error", {status: 500});
   }
 }
