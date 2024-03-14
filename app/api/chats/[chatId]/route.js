a2c576e26694e29827a7333df5bca0df3d7ae5ec
Eli Guy <eliguy343@gmail.com>
2024-03-14 23:18:27 +0200

    update seenby api

--- app/api/chats/[chatId]/route.js
+++ app/api/chats/[chatId]/route.js
@@ -27,3 +27,26 @@ export const GET = async (req, {params}) => {
     return new Response("Failed to get chat details", {status: 500});
   }
 }
+
+export const POST = async (req, {params}) => {
+  try {
+    const {chatId} = params;
+    const body = await req.json();
+    const {currentUserId} =  body;
+  
+    await Message.UpdateMany(
+      {chat: chatId},
+      {$addToSet: {seenBy: currentUserId}},
+      {new: true}
+    )
+    .populate({
+      path: "sender seenBy",
+      model: User,
+    }).exec();
+  
+    return new Response("Seen All messages by current User", {status: 200});
+  } catch (err) {
+    console.log(err);
+    return new Response("Failed to update seenby", {status: 500});
+  }
+}
