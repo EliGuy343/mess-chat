eda7de081a52771d44a91b5dc995d29ab59cd353
Eli Guy <eliguy343@gmail.com>
2024-03-15 15:05:59 +0200

    seen messages

--- app/(root)/chats/[chatId]/page.jsx
+++ app/(root)/chats/[chatId]/page.jsx
@@ -3,6 +3,7 @@ import ChatDetails from "@components/ChatDetails";
 import ChatList from "@components/ChatList";
 import { useSession } from "next-auth/react";
 import { useParams } from "next/navigation"
+import { useEffect } from "react";
 
 //TODO: Server side rendering
 
@@ -10,6 +11,30 @@ const ChatPage = () => {
   const {chatId} = useParams();
   const {data: session}  = useSession();
   const currentUser = session?.user;
+
+  const sendMessages = async () => {
+    try {
+      await fetch(`/api/chats/${chatId}`, {
+        method:"POST",
+        headers:{
+          "Content-Type": "application/json"
+        },
+        body:JSON.stringify({
+          currentChatId: currentUser._id
+        })
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    if (currentUser && chatId) {
+      sendMessages();
+    }
+  }, []);
+  
+
   return (
     <div className="main-container">
       <div className="w-1/3 max-lg:hidden">
